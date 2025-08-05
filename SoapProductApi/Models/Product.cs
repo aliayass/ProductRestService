4203d527b8bd658c76456a34b5e63728489904e4
Ali Ayas <121394375+aliayass@users.noreply.github.com>
2025-08-05 13:17:22 +0300

    Delete SoapProductApi directory

--- SoapProductApi/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Product
-{
-    public int Id { get; set; }
-    public string? Renk { get; set; }
-    public int ItemId { get; set; }
-    public string? Barkod { get; set; }
-    public string? Beden { get; set; }
-    public decimal Price { get; set; }
-}
